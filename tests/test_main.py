4ff0278cc778b6495681862483e9c8853003a416
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-02-26 20:11:52 +0700

    code changed (task_2)

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,8 +3,8 @@ from main import Product
 import os
 
 def test_name():
-    name1 = Product("СерыйХолодильник3000", 10000, 5)
-    assert name1.name == 'Exception: Длина наименования товара превышает 10 допустимых символов.'
+    name1 = Product("Серыйхолодильник3000", 10000, 5)
+    assert name1.name == 'Серыйхолодильник3000'
 
 
 def test_is_integer():
@@ -13,9 +13,9 @@ def test_is_integer():
     assert Product.is_integer_num(5.5) is False
 
 def test_instantiate_from_csv():
-    Product.instantiate_from_csv(os.path.join("tests", "test.csv"))
-    item = Product.all[-1]
-    assert item.name == 'Мышка'
-    assert item.price == '50'
-    assert item.quantity == '5'
+    item = Product.instantiate_from_csv(os.path.join("tests", "test.csv"))
+
+    assert item.name == 'Смартфон'
+    assert item.price == 100
+    assert item.quantity == 1
 
